ad5ed2935c6c999a36cc35257f204f0115916a87
root <root@AC1063>
2024-11-21 14:44:09 +0800

    modify first

--- pkg/controller/master/rancher/embedded_rancher.go
+++ pkg/controller/master/rancher/embedded_rancher.go
@@ -17,8 +17,8 @@ import (
 )
 
 var UpdateRancherUISettings = map[string]string{
-	"ui-pl":    "Harvester",
-	"ui-brand": "harvester",
+	"ui-pl":    "NOKIA-HCI",
+	"ui-brand": "nokia-hci",
 }
 
 func (h *Handler) RancherSettingOnChange(_ string, setting *rancherv3api.Setting) (*rancherv3api.Setting, error) {
